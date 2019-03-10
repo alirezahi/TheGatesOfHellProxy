4e2a46daa4d0389985020525b21a51484c62a77e
alirezahi <alireza97hi@gmail.com>
2019-03-10 18:45:59 +0330

    change app.js

--- app/src/App.js
+++ app/src/App.js
@@ -8,20 +8,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-          </p>
-          <div className="alert alert-sucess">
-          </div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div>
+          
+        </div>
       </div>
     );
   }
