83b52bc6ac7545ecb996d0ee20c857a21e7d7d89
alirezahi <alireza97hi@gmail.com>
2019-03-10 22:53:07 +0330

    check server host

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 var http = require('http');
 
-http.createServer(onRequest).listen(3000);
+http.createServer(onRequest).listen(8486);
 
 function onRequest(client_req, client_res) {
+  let hostname = client_req.headers.host
+  let port = 80
+  if(client_req.headers.host.includes(':')){
+    hostname = client_req.headers.host.split(':')[0]
+    port = client_req.headers.host.split(':')[1]
+  }
+  if(hostname == 'localhost' && port == '8486'){
+    client_res.setHeader('Content-Type', 'application/json');
+    client_res.end(JSON.stringify({ a: 1 }))
+  }
   var options = {
-    hostname: client_req.headers.host,
-    port: 80,
+    hostname: hostname,
+    port: port,
     path: client_req.url,
     method: client_req.method,
     headers: client_req.headers
